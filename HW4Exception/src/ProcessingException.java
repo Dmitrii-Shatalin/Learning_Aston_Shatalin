d0ed1f4dab46d2058420375cf7b3629f2153d5a5
Dmitrii-Shatalin <62507996+Dmitrii-Shatalin@users.noreply.github.com>
2025-04-27 16:14:26 +0300

    Task3

---
+++ HW4Exception/src/ProcessingException.java
@@ -0,0 +1,9 @@
+public class ProcessingException extends Exception {
+    public ProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ProcessingException(String message) {
+        super(message);
+    }
+}
